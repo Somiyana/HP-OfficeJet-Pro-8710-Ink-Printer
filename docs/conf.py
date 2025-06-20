56b71b4d472e6480484edffc6bfa6548a9c8bd41
Somiyana <somiyanamir@gmail.com>
2025-06-20 12:37:09 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,20 +5,4 @@ release = '1.0'
 extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
-
-document.addEventListener("DOMContentLoaded", function () {
-    var ze = document.createElement('script');
-    ze.id = 'ze-snippet';
-    ze.src = 'https://static.zdassets.com/ekr/snippet.js?key=7668c976-0fb7-47ee-8740-3e9115bdb10f';
-    document.body.appendChild(ze);
-});
- create a file name "chatbot.js" inside "_static" folder in "doc" folder
- 
- 
-html_js_files = [
-    'chatbot.js',
-]
  
